16b84a987722434b7f51a285b75e066297f0137d
cesarcarrizo <43326793+cesarcarrizo@users.noreply.github.com>
2020-08-04 15:02:40 -0600

    commited on 'dev'

--- rutas/index.js
+++ rutas/index.js
@@ -10,39 +10,16 @@ const router = express.Router();
 
 // database: objeto importado del modulo database que se pasa desde el servidor
 module.exports = (database) => {
+  let datosAdminRegistrado = [];
+  let datosProfRegistrado = [];
+  let tipoUsuario = 0;
+
   //////////////////////////// LOGIN ADMIN //////////////////////////////
   //ruta del inicio de sesion
   router.get("/loginA", (req, res) => {
     res.render("paginas/loginA");
   });
-  router.post("/loginA", async (req, res) => {
-    try {
-      // se consulta primero si el usuario digitado existe
-      let registro = await database.validarUsuario(
-        req.body.ced,
-        req.body.passwd
-      );
-      if (registro[0].tipo_usu == 0) {
-        // si esta en estado pendiente redirige al home
-        res.redirect("/");
-      }
-      if (registro[0].tipo_usu == 1) {
-        // si es un administrador:
-        datosAdminRegistrado = await database.select(
-          "administradores",
-          "ced_adm_pk",
-          req.body.ced
-        );
-        res.redirect("paginas/menuAdmin");
-      }
-      if (registro[0].tipo_usu == 2) {
-        // si es profesional redirije al profesional.
-      }
-    } catch (err) {
-      console.log(err);
-      res.send("Usuario no existente.");
-    }
-  });
+  router.post("/loginA", async (req, res) => {});
 
   //////////////////////////// MENU ADMIN //////////////////////////////
   //ruta del menu del admin
@@ -50,7 +27,7 @@ module.exports = (database) => {
     res.render("paginas/menuAdmin");
   });
 
-  //////////////////////////// REGISRO ADMIN //////////////////////////////
+  //////////////////////////// REGISTRO ADMIN //////////////////////////////
   // ruta del registro para ser administrador
   router.get("/registroAdmin", (req, res) => {
     res.render("paginas/registroAdmin");
@@ -97,29 +74,6 @@ module.exports = (database) => {
     res.render("paginas/loginP");
   });
 
-  let datosAdminRegistrado = [];
-  let datosProfRegistrado = [];
-  let tipoUsuario = 0;
-
-  // ruta de la pagina servicios
-  router.get("/servicios", (req, res) => {
-    res.render("paginas/servicios", {
-      servicios: [
-        "Networking",
-        "PC Support & Instalación",
-        "Back Up SDD & HDD",
-        "Recuperación de datos",
-        "Soporte Remoto",
-        "Soporte Smartphone",
-        "Reparaciones PC/Mac",
-        "Consultorias",
-        "IT",
-        "Training",
-        "Infosec",
-      ],
-    });
-  });
-
   //////////////////////////// REGISTRO PROFESIONAL /////////////////////
   // ruta para registrar una solicitud nueva para ser profesional
   router.get("/registroProf", (req, res) => {
@@ -161,13 +115,13 @@ module.exports = (database) => {
     res.redirect("/");
   });
 
-  //////////////////////////// LOGIN PERSONAL //////////////////////////////
+  //////////////////////////// PERSONAL //////////////////////////////
   router.get("/personal", (req, res) => {
     res.render("paginas/personal");
     // fix
   });
 
-  //////////////////////////// LOGIN ABOUT //////////////////////////////
+  //////////////////////////// ABOUT //////////////////////////////
   // ruta para sobre nosotros
   router.get("/about", (req, res) => {
     res.render("paginas/about");
@@ -179,10 +133,30 @@ module.exports = (database) => {
     res.render("paginas/index");
   });
 
-  //////////////////////////// ELECCION ////////////////////////////////
+  //////////////////////////// ELECCION DE LOGIN ////////////////////////////////
   //ruta de la pagina eleccion (especifica que usuario va a iniciar sesion)
   router.get("/eleccion", (req, res) => {
     res.render("paginas/eleccion");
+  });
+
+  //////////////////////////// SERVICIOS ///////////////////////////////////
+  // ruta de la pagina servicios
+  router.get("/servicios", (req, res) => {
+    res.render("paginas/servicios", {
+      servicios: [
+        "Networking",
+        "PC Support & Instalación",
+        "Back Up SDD & HDD",
+        "Recuperación de datos",
+        "Soporte Remoto",
+        "Soporte Smartphone",
+        "Reparaciones PC/Mac",
+        "Consultorias",
+        "IT",
+        "Training",
+        "Infosec",
+      ],
+    });
   });
 
   return router;
